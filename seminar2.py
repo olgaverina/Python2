5fda1f64fbb9a36ee191a488918bf4e4f4037ef8
Verina Olga <verinaolga@gmail.com>
2022-06-10 22:36:13 +0300

    second seminar

---
+++ seminar2.py
@@ -0,0 +1,63 @@
+#1. Реализуйте алгоритм перемешивания списка.
+from curses.ascii import isdigit
+
+import os
+os.system('cls||clear')
+
+import random
+from random import randint
+
+# list = [randint(-10,10) for i in range(20)]
+# # print(list)
+
+# for i in list:
+#     temp = list[i]
+#     temp_i = i
+#     temp_rand = randint(range(20))
+#     list.insert(i, )
+# # random.shuffle(list)
+# print(list)
+
+
+#2. Реализовать алгоритм задания случайного числа без генератора случайных чисел
+# import time
+# seconds = int(time.time())
+# print(seconds)
+# miliseconds = int(time.time()*60)
+# print(miliseconds)
+# print(miliseconds/10)
+
+#3. Задайте список из n чисел последовательности (1+1/n)**n и выведите на экран их сумму.
+
+# num = int(input("Enter the number >> "))
+# array = []
+# sum = 0
+# for i in range(1, num):
+#     array.append((1 + 1/i)**i)
+#     print((1 + 1/i)**i)
+#     sum += (1 + 1/i)**i
+
+# print(round(sum,num))
+
+#4. Задайте список. Напишите программу, которая определит, 
+# присутствует ли в заданном списке строк некое число
+
+num = int(input("Enter the number >> "))
+array = ['geek', 'brains4', '5five', '3friends', '1234']
+ 
+for i in array:
+    for j in i:
+        if (j.isdigit() and int(j) == num):
+            print(f'{i} - true')
+            break
+        else:
+            print(f'{i} - false')
+            break
+        continue
+
+        
+
+    
+
+
+
