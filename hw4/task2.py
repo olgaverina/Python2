a91aab5e3da8287e7c57dfd779c1a9e2630f950c
Verina Olga <verinaolga@gmail.com>
2022-06-21 00:48:03 +0300

    hw4

--- hw4/task2.py
+++ hw4/task2.py
@@ -5,13 +5,26 @@
 #  [5, 2, 3, 4, 6, 1, 7] => [2, 3, 4, 6, 7]
 #  Порядок элементов менять нельзя
 
-array = [1, 5, 2, 3, 4, 6, 1, 7]
-sorted_arr = []
+array = [1, 5, 2, 3, 4, 6, 1, 7, 10]
 
-for i in array:
-    for j in array:
-        if i < 
-        sorted_arr.append(a)
-    print()
 
-print(sorted_arr)
+max_arr = []
+max_len_array = 0
+
+for i in range(0, len(array)):
+    current_arr = []
+    current_arr.append(array[i])
+    current_len_arr = 1               #
+    max = array[i]
+    for j in range(1, len(array)):
+        if (((i + j < len(array)) and array[i + j] > max)):
+            current_arr.append(array[i + j])
+            max = array[i + j]
+            current_len_arr += 1
+    if (max_len_array < current_len_arr):
+        max_len_array = current_len_arr
+        max_arr = current_arr
+
+
+print(f"Max array - {max_arr}")
+
